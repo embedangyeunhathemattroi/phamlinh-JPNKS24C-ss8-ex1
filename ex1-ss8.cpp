7a4638f0f9a260308a4712ae3d9dd541f18a593c
empedangiunhatptit <phamngoclinhtq2006@gmail.com>
2024-11-28 20:15:40 +0700

    Add files via upload

---
+++ ex1-ss8.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int listagefamily[2] [3] = { {2,70,35},{75,43,18} };
+	printf("tuoi cua em be la :%d \n",listagefamily[0] [0]);
+	printf("tuoi cua ba ngoai la :%d \n",listagefamily[0] [1]);
+	printf("tuoi cua di ruot la :%d \n",listagefamily[0] [2]);
+	printf("tuoi cua ong ngoai la :%d \n",listagefamily[1] [0]);
+	printf("tuoi cua me la :%d \n",listagefamily[1] [1]);
+	printf("tuoi cua toi la :%d \n",listagefamily[1] [2]);
+	return 0;
+}
+	
+	
+	
+	
+	
